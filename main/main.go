313cebacbd3bb3b462d64b210b3c618dd16dc43d
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-14 13:26:19 -0600

    Set actual window size

--- main/main.go
+++ main/main.go
@@ -5,8 +5,8 @@ import (
 	"log"
 )
 
-const windowWidth = 640
-const windowHeight = 480
+const windowWidth = 1280
+const windowHeight = 720
 
 type Game struct{}
 
@@ -18,14 +18,14 @@ func (g *Game) Draw(*ebiten.Image) {
 }
 
 func (g *Game) Layout(int, int) (screenWidth int, screenHeight int) {
-	return 320, 240
+	return windowWidth, windowHeight
 }
 
 func main() {
 	game := &Game{}
 
 	ebiten.SetWindowSize(windowWidth, windowHeight)
-	ebiten.SetWindowTitle("Your game's title")
+	ebiten.SetWindowTitle("Dungeon MST")
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
