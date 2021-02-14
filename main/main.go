79bf97f88008b7866790577aedf1a1e838789088
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-14 00:24:29 -0600

    Get started with the game window

--- main/main.go
+++ main/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/hajimehoshi/ebiten"
+	"log"
+)
+
+const windowWidth = 640
+const windowHeight = 480
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(*ebiten.Image) {
+}
+
+func (g *Game) Layout(int, int) (screenWidth int, screenHeight int) {
+	return 320, 240
+}
 
 func main() {
-	fmt.Println("dungeon-mst")
+	game := &Game{}
+
+	ebiten.SetWindowSize(windowWidth, windowHeight)
+	ebiten.SetWindowTitle("Your game's title")
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
