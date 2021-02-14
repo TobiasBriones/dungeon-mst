811088a71e90d5fc850e25ae2c0fb0cdb667458d
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-14 14:07:13 -0600

    Put runner sprite animation

--- main.go
+++ main.go
@@ -1,33 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/examples/resources/images"
+	"image"
+	_ "image/png"
 	"log"
 )
 
-const windowWidth = 1280
-const windowHeight = 720
+const (
+	screenWidth  = 600
+	screenHeight = 360
 
-type Game struct{}
+	frameOX     = 0
+	frameOY     = 32
+	frameWidth  = 32
+	frameHeight = 32
+	frameNum    = 8
+)
+
+var (
+	runnerImage *ebiten.Image
+)
+
+type Game struct {
+	count int
+}
 
 func (g *Game) Update() error {
+	g.count++
 	return nil
 }
 
-func (g *Game) Draw(*ebiten.Image) {
+func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	i := (g.count / 5) % frameNum
+	sx, sy := frameOX+i*frameWidth, frameOY
+	rect := image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)
+	screen.DrawImage(runnerImage.SubImage(rect).(*ebiten.Image), op)
 }
 
-func (g *Game) Layout(int, int) (screenWidth int, screenHeight int) {
-	return windowWidth, windowHeight
+func (g *Game) Layout(int, int) (int, int) {
+	return screenWidth, screenHeight
 }
 
 func main() {
-	game := &Game{}
+	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
 
-	ebiten.SetWindowSize(windowWidth, windowHeight)
+	if err != nil {
+		log.Fatal(err)
+	}
+	runnerImage = ebiten.NewImageFromImage(img)
+
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Dungeon MST")
 
-	if err := ebiten.RunGame(game); err != nil {
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
