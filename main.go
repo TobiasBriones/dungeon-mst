0ee353f01357aaa6795f348bdd7e1c2839a8a830
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-20 21:46:57 -0600

    Start drawing dungeons

--- main.go
+++ main.go
@@ -57,6 +57,7 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	drawBg(screen)
+	drawSomeDungeons(screen)
 	drawRunner(g, screen)
 }
 
@@ -76,38 +77,83 @@ type Rect struct {
 }
 
 type Dungeon struct {
-	Rect  Rect
-	image *ebiten.Image
+	rect        Rect
+	brickImage  *ebiten.Image
+	brickYImage *ebiten.Image
 }
 
 func (d *Dungeon) Width() int {
-	return d.Rect.Right - d.Rect.Left
+	return d.rect.Right - d.rect.Left
 }
 
 func (d *Dungeon) Height() int {
-	return d.Rect.Bottom - d.Rect.Top
+	return d.rect.Bottom - d.rect.Top
 }
 
 func (d *Dungeon) Cx() int {
-	return d.Rect.Left + d.Width()/2
+	return d.rect.Left + d.Width()/2
 }
 
 func (d *Dungeon) Cy() int {
-	return d.Rect.Top + d.Height()/2
+	return d.rect.Top + d.Height()/2
 }
 
 func (d *Dungeon) Overlaps(other Dungeon, margin int) bool {
-	xo := (d.Rect.Left-margin) <= (other.Rect.Right+margin) &&
-		(d.Rect.Right+margin) >= (other.Rect.Left-margin)
+	xo := (d.rect.Left-margin) <= (other.rect.Right+margin) &&
+		(d.rect.Right+margin) >= (other.rect.Left-margin)
 
-	yo := (d.Rect.Top-margin) <= (other.Rect.Bottom+margin) &&
-		(d.Rect.Bottom+margin) >= (other.Rect.Top-margin)
+	yo := (d.rect.Top-margin) <= (other.rect.Bottom+margin) &&
+		(d.rect.Bottom+margin) >= (other.rect.Top-margin)
 
 	return xo && yo
 }
 
-func NewDungeon(rect Rect) Dungeon {
-	return Dungeon{rect, nil}
+func (d *Dungeon) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	sizeX := d.Width() / 128
+	sizeY := d.Height() / 128
+	op.GeoM.Translate(float64(d.rect.Left), float64(d.rect.Top))
+
+	for i := 0; i < sizeX; i++ {
+		screen.DrawImage(d.brickImage, op)
+		op.GeoM.Translate(128, 0)
+	}
+
+	op.GeoM.Reset()
+	op.GeoM.Translate(float64(d.rect.Left), float64(d.rect.Bottom-24))
+	for i := 0; i < sizeX; i++ {
+		screen.DrawImage(d.brickImage, op)
+		op.GeoM.Translate(128, 0)
+	}
+
+	op.GeoM.Reset()
+	op.GeoM.Translate(float64(d.rect.Left), float64(d.rect.Top))
+	for i := 0; i < sizeY; i++ {
+		screen.DrawImage(d.brickYImage, op)
+		op.GeoM.Translate(0, 128)
+	}
+
+	op.GeoM.Reset()
+	op.GeoM.Translate(float64(d.rect.Right-24), float64(d.rect.Top))
+	for i := 0; i < sizeY; i++ {
+		screen.DrawImage(d.brickYImage, op)
+		op.GeoM.Translate(0, 128)
+	}
+}
+
+func NewDungeon(x0 int, y0 int, sizeX int, sizeY int) Dungeon {
+	brickImg, _, err := ebitenutil.NewImageFromFile("./assets/brick.png")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	brickYImg, _, err := ebitenutil.NewImageFromFile("./assets/brick_y.png")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	rect := Rect{x0, y0, x0 + sizeX*128, y0 + sizeY*128}
+	return Dungeon{rect, brickImg, brickYImg}
 }
 
 func (g *Game) Layout(int, int) (int, int) {
@@ -204,4 +250,21 @@ func drawRunner(g *Game, screen *ebiten.Image) {
 	op.GeoM.Scale(runner.Scale, runner.Scale)
 	op.GeoM.Translate(float64(pos.X), float64(pos.Y))
 	screen.DrawImage(runnerImage.SubImage(rect).(*ebiten.Image), op)
+}
+
+func drawSomeDungeons(screen *ebiten.Image) {
+	dungeons := genSomeDungeons()
+
+	for _, dungeon := range dungeons {
+		dungeon.Draw(screen)
+	}
+}
+
+func genSomeDungeons() []Dungeon {
+	return []Dungeon{
+		NewDungeon(0, 0, 1, 1),
+		NewDungeon(20, 540, 4, 1),
+		NewDungeon(200, 140, 3, 2),
+		NewDungeon(350, 90, 4, 1),
+	}
 }
