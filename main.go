14ccf4f82a9b30ac3086ed44d1de6714936ce8c8
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-20 22:17:42 -0600

    Move Runner source code to the model package

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"dungeon-mst/model"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
-	"github.com/hajimehoshi/ebiten/examples/resources/images"
-	"image"
 	_ "image/png"
 	"log"
 	"math/rand"
@@ -17,99 +14,36 @@ import (
 const (
 	screenWidth  = 1280
 	screenHeight = 720
-
-	frameOX     = 0
-	frameOY     = 32
-	frameWidth  = 32
-	frameHeight = 32
-	frameNum    = 8
 )
 
 var (
-	bgImage     *ebiten.Image
-	runnerImage *ebiten.Image
+	bgImage *ebiten.Image
 )
 
 type Game struct {
-	count  int
-	runner Runner
+	runner model.Runner
 }
 
 func (g *Game) Update() error {
-	g.count++
-
-	for k := ebiten.Key(0); k <= ebiten.KeyMax; k++ {
-		if ebiten.IsKeyPressed(k) {
-			switch k {
-			case ebiten.KeyUp, ebiten.KeyW:
-				g.runner.Pos.Y--
-			case ebiten.KeyDown, ebiten.KeyS:
-				g.runner.Pos.Y++
-			case ebiten.KeyLeft, ebiten.KeyA:
-				g.runner.Pos.X--
-			case ebiten.KeyRight, ebiten.KeyD:
-				g.runner.Pos.X++
-			}
-		}
-	}
-	g.runner.Normalize()
+	g.runner.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	drawBg(screen)
+	screen.DrawImage(bgImage, nil)
 	drawSomeDungeons(screen)
-	drawRunner(g, screen)
+	g.runner.Draw(screen)
 }
 
 func NewGame() Game {
-	runner := NewRunner()
+	runner := model.NewRunner()
 	return Game{
-		count:  0,
 		runner: runner,
 	}
 }
 
 func (g *Game) Layout(int, int) (int, int) {
 	return screenWidth, screenHeight
-}
-
-type Runner struct {
-	Pos   image.Point
-	Scale float64
-}
-
-func (r *Runner) Center() {
-	r.Pos.X = int(-(frameWidth * r.Scale) / 2)
-	r.Pos.Y = int(-(frameHeight * r.Scale) / 2)
-	r.Pos.X += screenWidth / 2
-	r.Pos.Y += screenHeight / 2
-}
-
-func (r *Runner) Normalize() {
-	pos := &r.Pos
-
-	if pos.X < 0 {
-		pos.X = 0
-	}
-	if pos.X > screenWidth-int(frameWidth*r.Scale) {
-		pos.X = screenWidth - int(frameWidth*r.Scale)
-	}
-	if pos.Y < 0 {
-		pos.Y = 0
-	}
-	if pos.Y > screenHeight-int(frameHeight*r.Scale) {
-		pos.Y = screenHeight - int(frameHeight*r.Scale)
-	}
-}
-
-func NewRunner() Runner {
-	runner := Runner{
-		Pos:   image.Point{},
-		Scale: 2,
-	}
-	runner.Center()
-	return runner
 }
 
 func main() {
@@ -125,7 +59,6 @@ func main() {
 
 func init() {
 	loadBg()
-	loadRunner()
 }
 
 func loadBg() {
@@ -138,32 +71,6 @@ func loadBg() {
 		log.Fatal(err)
 	}
 	bgImage = bgImg
-}
-
-func loadRunner() {
-	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	runnerImage = ebiten.NewImageFromImage(img)
-}
-
-func drawBg(screen *ebiten.Image) {
-	screen.DrawImage(bgImage, nil)
-}
-
-func drawRunner(g *Game, screen *ebiten.Image) {
-	runner := g.runner
-	pos := runner.Pos
-	op := &ebiten.DrawImageOptions{}
-	i := (g.count / 5) % frameNum
-	sx, sy := frameOX+i*frameWidth, frameOY
-	rect := image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)
-
-	op.GeoM.Scale(runner.Scale, runner.Scale)
-	op.GeoM.Translate(float64(pos.X), float64(pos.Y))
-	screen.DrawImage(runnerImage.SubImage(rect).(*ebiten.Image), op)
 }
 
 func drawSomeDungeons(screen *ebiten.Image) {
---
+++ model/runner.go
@@ -0,0 +1,100 @@
+package model
+
+import (
+	"bytes"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/examples/resources/images"
+	"image"
+	"log"
+)
+
+const (
+	screenWidth  = 1280
+	screenHeight = 720
+
+	frameOX     = 0
+	frameOY     = 32
+	frameWidth  = 32
+	frameHeight = 32
+	frameNum    = 8
+)
+
+type Runner struct {
+	Pos   image.Point
+	Scale float64
+	count int
+	image *ebiten.Image
+}
+
+func (r *Runner) Center() {
+	r.Pos.X = int(-(frameWidth * r.Scale) / 2)
+	r.Pos.Y = int(-(frameHeight * r.Scale) / 2)
+	r.Pos.X += screenWidth / 2
+	r.Pos.Y += screenHeight / 2
+}
+
+func (r *Runner) Normalize() {
+	pos := &r.Pos
+
+	if pos.X < 0 {
+		pos.X = 0
+	}
+	if pos.X > screenWidth-int(frameWidth*r.Scale) {
+		pos.X = screenWidth - int(frameWidth*r.Scale)
+	}
+	if pos.Y < 0 {
+		pos.Y = 0
+	}
+	if pos.Y > screenHeight-int(frameHeight*r.Scale) {
+		pos.Y = screenHeight - int(frameHeight*r.Scale)
+	}
+}
+
+func (r *Runner) Update() {
+	r.count++
+
+	for k := ebiten.Key(0); k <= ebiten.KeyMax; k++ {
+		if ebiten.IsKeyPressed(k) {
+			switch k {
+			case ebiten.KeyUp, ebiten.KeyW:
+				r.Pos.Y--
+			case ebiten.KeyDown, ebiten.KeyS:
+				r.Pos.Y++
+			case ebiten.KeyLeft, ebiten.KeyA:
+				r.Pos.X--
+			case ebiten.KeyRight, ebiten.KeyD:
+				r.Pos.X++
+			}
+		}
+	}
+	r.Normalize()
+}
+
+func (r *Runner) Draw(screen *ebiten.Image) {
+	pos := r.Pos
+	op := &ebiten.DrawImageOptions{}
+	i := (r.count / 5) % frameNum
+	sx, sy := frameOX+i*frameWidth, frameOY
+	rect := image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)
+
+	op.GeoM.Scale(r.Scale, r.Scale)
+	op.GeoM.Translate(float64(pos.X), float64(pos.Y))
+	screen.DrawImage(r.image.SubImage(rect).(*ebiten.Image), op)
+}
+
+func NewRunner() Runner {
+	runner := Runner{
+		Pos:   image.Point{},
+		Scale: 2,
+		count: 0,
+	}
+	runner.Center()
+
+	img, _, err := image.Decode(bytes.NewReader(images.Runner_png))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	runner.image = ebiten.NewImageFromImage(img)
+	return runner
+}
