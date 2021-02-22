1ffdf5fa54cffb5b3971aa55f3296931a32b5f61
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-21 22:15:59 -0600

    Temporarily draw paths to neighbour dungeons while developing the algorithms

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"dungeon-mst/ai"
 	"dungeon-mst/model"
 	"fmt"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"image/color"
 	_ "image/png"
 	"log"
+	"math"
 	"math/rand"
 	"strconv"
 	"time"
@@ -51,8 +52,8 @@ func (g *Game) Layout(int, int) (int, int) {
 
 func main() {
 	game := NewGame()
-	dungeons = ai.GenerateDungeons(getSize())
-	//dungeons = genSomeDungeons()
+	//dungeons = ai.GenerateDungeons(getSize())
+	dungeons = genSomeDungeons()
 
 	testRectIntersect()
 	ebiten.SetWindowSize(screenWidth, screenHeight)
@@ -88,16 +89,53 @@ func loadBg() {
 func drawSomeDungeons(screen *ebiten.Image) {
 	for _, dungeon := range dungeons {
 		dungeon.Draw(screen)
+
+		for _, neighbor := range dungeon.Neighborhood {
+			center := dungeon.Center()
+			w1 := int(math.Abs(float64(center.X - neighbor.Cx())))
+			h1 := 12
+			w2 := 12
+			h2 := int(math.Abs(float64(center.Y - neighbor.Cy())))
+			line1 := ebiten.NewImage(w1, h1)
+			line2 := ebiten.NewImage(w2, h2)
+			ltr := center.X-neighbor.Cx() < 0
+			ttb := center.Y-neighbor.Cy() < 0
+
+			op := &ebiten.DrawImageOptions{}
+
+			line1.Fill(color.Gray{})
+			line2.Fill(color.Gray{})
+
+			if ltr {
+				op.GeoM.Translate(float64(center.X), float64(center.Y))
+			} else {
+				op.GeoM.Translate(float64(neighbor.Cx()), float64(center.Y))
+			}
+			screen.DrawImage(line1, op)
+
+			op.GeoM.Reset()
+			if ttb {
+				op.GeoM.Translate(float64(dungeon.Cx()), float64(center.Y))
+			} else {
+				op.GeoM.Translate(float64(center.X), float64(neighbor.Cy()))
+			}
+			screen.DrawImage(line2, op)
+		}
 	}
 }
 
 func genSomeDungeons() []model.Dungeon {
-	return []model.Dungeon{
+	dungeons := []model.Dungeon{
 		model.NewDungeon(model.Point{}, model.DimensionFactor{Width: 1, Height: 1}),
 		model.NewDungeon(model.Point{X: 20, Y: 540}, model.DimensionFactor{Width: 4, Height: 1}),
 		model.NewDungeon(model.Point{X: 200, Y: 140}, model.DimensionFactor{Width: 3, Height: 2}),
 		model.NewDungeon(model.Point{X: 350, Y: 90}, model.DimensionFactor{Width: 4, Height: 1}),
 	}
+	dungeons[0].Neighborhood = []*model.Dungeon{&dungeons[1], &dungeons[2]}
+	dungeons[1].Neighborhood = []*model.Dungeon{&dungeons[0], &dungeons[2]}
+	dungeons[2].Neighborhood = []*model.Dungeon{&dungeons[0], &dungeons[1], &dungeons[3]}
+	dungeons[3].Neighborhood = []*model.Dungeon{&dungeons[2]}
+	return dungeons
 }
 
 func testRectIntersect() {
