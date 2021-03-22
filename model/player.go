e1240c68153d162a77717cc48582d424bbebf871
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-03-22 00:10:17 -0600

    Draw player name

--- model/player.go
+++ model/player.go
@@ -6,7 +6,34 @@ package model
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+	"image/color"
+	"log"
 )
+
+var (
+	mplusNormalFont font.Face
+)
+
+func init() {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    12,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Player struct {
 	name           string
@@ -41,6 +68,15 @@ func (p *Player) Update() {
 
 func (p *Player) Draw(screen *ebiten.Image) {
 	p.character.Draw(screen)
+	p.drawName(screen)
+}
+
+func (p *Player) drawName(screen *ebiten.Image) {
+	name := p.name
+	character := p.character
+	x := character.Rect.Left()
+	y := character.Rect.Top()
+	text.Draw(screen, name, mplusNormalFont, x, y, color.Black)
 }
 
 func NewPlayer(name string) Player {
