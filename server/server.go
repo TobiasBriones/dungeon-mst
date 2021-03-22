07cc0aecadc61dcd4218a244b7ecc0d0fd2a8c11
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-03-22 12:17:09 -0600

    Create server module and sample ws-server

--- server/server.go
+++ server/server.go
@@ -2,4 +2,73 @@
  * Copyright (c) 2021 Tobias Briones. All rights reserved.
  */
 
-package server
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+type hub struct {
+	clients   map[*client]bool
+	broadcast chan []byte
+}
+
+type client struct {
+	conn *websocket.Conn
+	send chan []byte
+	hub  *hub
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+func main() {
+	setRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func setRoutes() {
+	http.HandleFunc("/", wsHandler)
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected...")
+
+	err = ws.WriteMessage(1, []byte("This is the server response"))
+	if err != nil {
+		log.Println(err)
+	}
+	listenConn(ws)
+}
+
+func listenConn(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(p))
+
+		err = conn.WriteMessage(messageType, p)
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
