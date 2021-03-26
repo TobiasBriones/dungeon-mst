8bab551e2f36937fc6fc6e0828d29bd1bc7eb314
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-03-25 20:43:59 -0600

    Implement function: NewImageFromAssets

--- game/model/utils.go
+++ game/model/utils.go
@@ -3,3 +3,18 @@
  */
 
 package model
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+)
+
+func NewImageFromAssets(name string) *ebiten.Image {
+	image, _, err := ebitenutil.NewImageFromFile("./assets/" + name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return image
+}
