721f5ec828fe2f46e5cbd4d1f151b5f5157f824f
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-03-27 23:47:38 -0600

    Add some documentation

--- game/main.go
+++ game/main.go
@@ -8,6 +8,17 @@ import (
 	_ "image/png"
 )
 
+// This is a prototype version of the game for a proof-of-concept purpose.
+// I will obviously refactor a huge part of the codebase later since this
+// version is not stable.
+
+// Build the server from the server module and run it. (Set up the address).
+// Set the address also in the client package of the game module.
+// Open the file user.json at the root of this project to set your username.
+// Build the game from the game module and run it.
+
+// The server generates random matches each x seconds. Just open your game.
+
 func main() {
 	Run()
 }
