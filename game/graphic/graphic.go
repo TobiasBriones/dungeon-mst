ab0ef10fbf7545662940a9640f5ff05c4a595423
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-03-16 18:38:31 -0600

    Define struct Graphic on game.graphic
    It provides a high-level game API for image resources.

--- game/graphic/graphic.go
+++ game/graphic/graphic.go
@@ -3,3 +3,22 @@
 // This file is part of https://github.com/tobiasbriones/dungeon-mst
 
 package graphic
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+)
+
+type Graphic struct {
+	*ebiten.Image
+}
+
+func NewGraphicFromAssets(name string) *Graphic {
+	image, _, err := ebitenutil.NewImageFromFile("assets/" + name)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &Graphic{image}
+}
