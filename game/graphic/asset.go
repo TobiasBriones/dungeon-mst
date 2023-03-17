9784ad759cade93e0c7aac70b2be0f7929e0bd0b
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-03-16 18:47:01 -0600

    Add struct Graphics on game.graphic(asset) with support for entity Diamond
    It allows the mapping and loading of graphic resources.

--- game/graphic/asset.go
+++ game/graphic/asset.go
@@ -3,3 +3,39 @@
 // This file is part of https://github.com/tobiasbriones/dungeon-mst
 
 package graphic
+
+import "dungeon-mst/dungeon"
+
+type Graphics struct {
+	graphics map[dungeon.Entity]*Graphic
+}
+
+func (g *Graphics) Get(entity dungeon.Entity) *Graphic {
+	return g.graphics[entity]
+}
+
+// LoadGraphics loads the graphic assets of the game into memory.
+// These graphics only need to be loaded once, and be reused when drawing.
+// That is, there's a 1:n relation between a graphic and the instances of the
+// same entities that appear on the game.
+func LoadGraphics() *Graphics {
+	return &Graphics{map[dungeon.Entity]*Graphic{
+		dungeon.DiamondEntity: newGraphicFromDungeon(dungeon.DiamondEntity),
+	}}
+}
+
+const (
+	DiamondWidthPx  = 32
+	DiamondHeightPx = 26
+)
+
+var (
+	imageNameMatch = map[dungeon.Entity]string{
+		dungeon.DiamondEntity: "diamond.png",
+	}
+)
+
+func newGraphicFromDungeon(d dungeon.Entity) *Graphic {
+	name := imageNameMatch[d]
+	return NewGraphicFromAssets(name)
+}
