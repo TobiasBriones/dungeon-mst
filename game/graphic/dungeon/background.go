ba36a0735eefc0c43641f468cc95023cc1718e01
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-03-22 19:04:33 -0600

    Add graphic impl for background

--- game/graphic/dungeon/background.go
+++ game/graphic/dungeon/background.go
@@ -3,3 +3,48 @@
 // This file is part of https://github.com/tobiasbriones/dungeon-mst
 
 package dungeon
+
+import (
+	"dungeon-mst/game/graphic"
+	"math/rand"
+	"time"
+)
+
+type BackgroundGraphic uint8
+
+const (
+	Background1 BackgroundGraphic = iota
+	Background2
+	Background3
+)
+
+func (g BackgroundGraphic) Name() graphic.Name {
+	return map[BackgroundGraphic]graphic.Name{
+		Background1: "bg_1.png",
+		Background2: "bg_2.png",
+		Background3: "bg_3.png",
+	}[g]
+}
+
+type BackgroundGraphics map[BackgroundGraphic]*graphic.Graphic
+
+func LoadBackgroundGraphics(load graphic.Load) BackgroundGraphics {
+	return BackgroundGraphics{
+		Background1: load(Background1),
+		Background2: load(Background2),
+		Background3: load(Background3),
+	}
+}
+
+func NewBackgroundDrawing(
+	graphics EntityGraphics[BackgroundGraphic],
+	bg BackgroundGraphic,
+) graphic.Draw {
+	return graphic.NewDrawing(graphics.Get(bg), nil)
+}
+
+func GetRandomBackground() BackgroundGraphic {
+	rand.Seed(time.Now().UnixNano())
+	bgNumber := rand.Intn(3) + 1
+	return BackgroundGraphic(bgNumber - 1)
+}
