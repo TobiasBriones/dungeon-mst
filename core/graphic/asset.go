8e47ff283cd67d3c53e5fb4f8550d041a31206a6
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-03-24 18:18:27 -0600

    Move asset code to core.graphic(asset) with new path and docs

--- core/graphic/asset.go
+++ core/graphic/asset.go
@@ -3,3 +3,35 @@
 // This file is part of https://github.com/tobiasbriones/dungeon-mst
 
 package graphic
+
+import (
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
+)
+
+const (
+	AssetsDirPath = "asset/assets"
+)
+
+func GetAssetPath(name string) string {
+	return AssetsDirPath + name
+}
+
+// LoadGraphicFromAssets loads the given asset from the "asset/assets" directory
+// where the program is being executed.
+// Directory "asset" should be the model representation of the physical assets
+// in "asset/assets", i.e. a Go package that maps those graphics to an
+// application Graphic.
+func LoadGraphicFromAssets(name string) *Graphic {
+	path := GetAssetPath(name)
+	image, _, err := ebitenutil.NewImageFromFile(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &Graphic{image}
+}
+
+// Load defines a func type that loads a NamedGraphic from the FS to an
+// in-memory high-level Graphic.
+type Load func(g NamedGraphic) *Graphic
