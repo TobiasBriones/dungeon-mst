180a9a62881f78fd8a51785af1e15e37afe5ade4
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-03-24 18:21:46 -0600

    Move drawing code to core.graphic(drawing)

--- core/graphic/drawing.go
+++ core/graphic/drawing.go
@@ -3,3 +3,37 @@
 // This file is part of https://github.com/tobiasbriones/dungeon-mst
 
 package graphic
+
+import (
+	"dungeon-mst/core/geo"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// Draw defines an object that draws itself on a given canvas like the game
+// screen.
+type Draw interface {
+	Draw(screen *ebiten.Image)
+}
+
+// Drawing Defines a simple drawable object that has one Graphic and a geo.Rect
+// as position model.
+//
+// See Draw.
+type Drawing struct {
+	*Graphic
+	*geo.Rect
+}
+
+func NewDrawing(graphic *Graphic, rect *geo.Rect) Drawing {
+	return Drawing{
+		Graphic: graphic,
+		Rect:    rect,
+	}
+}
+
+func (d Drawing) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+
+	op.GeoM.Translate(float64(d.Left()), float64(d.Top()))
+	screen.DrawImage(d.Image, op)
+}
--- core/graphic/graphic.go
+++ core/graphic/graphic.go
@@ -5,26 +5,12 @@
 package graphic
 
 import (
-	"dungeon-mst/core/geo"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"log"
 )
 
 // Graphic defines a unit of asset (i.e. an image) of the game.
 type Graphic struct {
 	*ebiten.Image
-}
-
-// LoadGraphicFromAssets loads the given asset from the "assets" directory where
-// the program is being executed.
-func LoadGraphicFromAssets(name string) *Graphic {
-	image, _, err := ebitenutil.NewImageFromFile("assets/" + name)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &Graphic{image}
 }
 
 // Name defines the name of one asset. It should be the physical name of the
@@ -36,37 +22,4 @@ type Name string
 // See Name.
 type NamedGraphic interface {
 	Name() Name
-}
-
-// Load defines a func type that loads a NamedGraphic from the FS to an
-// in-memory high-level Graphic.
-type Load func(g NamedGraphic) *Graphic
-
-// Draw defines an object that draws itself on a given canvas like the game
-// screen.
-type Draw interface {
-	Draw(screen *ebiten.Image)
-}
-
-// Drawing Defines a simple drawable object that has one Graphic and a geo.Rect
-// as position model.
-//
-// See Draw.
-type Drawing struct {
-	*Graphic
-	*geo.Rect
-}
-
-func NewDrawing(graphic *Graphic, rect *geo.Rect) Drawing {
-	return Drawing{
-		Graphic: graphic,
-		Rect:    rect,
-	}
-}
-
-func (d Drawing) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-
-	op.GeoM.Translate(float64(d.Left()), float64(d.Top()))
-	screen.DrawImage(d.Image, op)
 }
